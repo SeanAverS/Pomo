c6455bafc542cbc1323f645e2ea25cea970c02fb
Sean <110581427+SeanAverS@users.noreply.github.com>
2023-02-21 22:31:31 -0800

    Set time for focus and rest periods

--- pomodoro.js
+++ pomodoro.js
@@ -5,12 +5,16 @@ function timeConvert(number) {
     let seconds = number % 60;
     seconds < 10 ? seconds = "0" + seconds : "error"; //display "08" not "8"
     time = minutes + ":" + seconds;
+    return time;
 };
 
 const playBtn = document.getElementById('play-button');
 playBtn.addEventListener('click', pomodoro);
 
+const focus = timeConvert(1500);
+const rest = timeConvert(300);
+
 function pomodoro() {
-    
+
 }
 
