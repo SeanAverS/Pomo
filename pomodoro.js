d7917f30c7e2b7b8d3aaf3e786050262759d6da4
Sean <110581427+SeanAverS@users.noreply.github.com>
2023-03-03 22:08:56 -0800

    Rearrange code for readability

--- pomodoro.js
+++ pomodoro.js
@@ -1,11 +1,39 @@
+//Convert work/break time to mm:ss format
 function convertTime(number) {
     let minutes = Math.floor(number / 60);
     let seconds = number % 60;
-    seconds < 10 ? seconds = "0" + seconds : "error"; //display "08" not "8"
+    //display "08" not "8" for ex.
+    seconds < 10 ? seconds = "0" + seconds : "error"; 
     let time = minutes + ":" + seconds;
     return time;
 };
 
+//Display total work time on page 
+const displayPomoTime = document.getElementById('total-time');
+let totalPomoTime = 0;
+
+function totalPomo() {
+    if (counter % 2 === 0) {
+        totalPomoTime += 1500; //add time to global var
+    } else {
+        totalPomoTime += 0;
+    } //console.log('total work: ' + totalPomoTime);
+      displayPomoTime.textContent = convertTime(totalPomoTime) + ' minutes so far. Great work!';
+}
+
+//Display work/break message after time ends
+const breakOrWorkMsg = document.getElementById('msg');
+breakOrWorkMsg.addEventListener('click', breakOrWork);
+
+function breakOrWork() {
+    if (counter % 2 === 0) {
+        breakOrWorkMsg.textContent = 'Break Time!';
+    } else {
+        breakOrWorkMsg.textContent = 'Work Time!';
+    }
+}
+
+//Set pomoTime var to work/break time based on global counter
 let counter = 0;
 let pomoTime = 5;
 
@@ -17,6 +45,7 @@ function setTime() {
     }
 }
 
+//Pomodoro Timer
 const timer = document.getElementById('timer');
 
 const playBtn = document.getElementById('play-button');
@@ -28,48 +57,23 @@ function pomodoro() {
             //display mm:ss format, but decrement time value
             timer.textContent = convertTime(pomoTime);
             pomoTime--;
-        } else {
+        } else { //once pomoTime === 0 
             clearInterval(currentTime);
             timer.textContent = convertTime(pomoTime);
-            console.log(counter);
+            //console.log('counter b4 increment: ' + counter);
             totalPomo();
             breakOrWork();
             counter++;
-            setTime(); //once pomoTime === 0 (times up)
-            console.log('counter: ' + counter);
+            setTime(); 
+            //console.log('counter after increment: ' + counter);
         }
     }, 1000);
 }
 
-const displayPomoTime = document.getElementById('total-time');
-
-let totalPomoTime = 0;
-
-function totalPomo() {
-    if (counter % 2 === 0) {
-        totalPomoTime += 1500; //add total work time to global var
-    } else {
-        totalPomoTime += 0;
-    } //console.log('total work: ' + totalPomoTime);
-      displayPomoTime.textContent = convertTime(totalPomoTime) + ' minutes so far. Great work!';
-}
-
+//Pause Timer
 const pauseBtn = document.getElementById('pause-button');
 pauseBtn.addEventListener('click', pauseButton);
 
 function pauseButton() {
     clearInterval(currentTime);
-}
-
-const breakOrWorkMsg = document.getElementById('msg');
-
-breakOrWorkMsg.addEventListener('click', breakOrWork);
-
-function breakOrWork() {
-    if (counter % 2 === 0) {
-        breakOrWorkMsg.textContent = 'Break Time!';
-    } else {
-        breakOrWorkMsg.textContent = 'Work Time!';
-    }
-}
-
+} 
